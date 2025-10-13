96614deead701420f4b2f5f459d75c56fd8c4a62
krishxkapoor <krishxkapoor@gmail.com>
2025-10-14 01:08:00 +0530

    first

---
+++ src/main/java/com/Kapoor/journalApp/Controller/EntryRestController.java
@@ -0,0 +1,72 @@
+package com.Kapoor.journalApp.Controller;
+
+import com.Kapoor.journalApp.entity.JournalEntry;
+import com.Kapoor.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/journal")
+public class EntryRestController {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @GetMapping
+    public ResponseEntity<List<JournalEntry>> getAll() {
+        List<JournalEntry> entries = journalEntryService.getAll();
+        if(entries!= null && entries.isEmpty()){
+            return new ResponseEntity<>(entries, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry journalEntry) {
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(journalEntry);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{myId}")
+    public ResponseEntity<JournalEntry> getEntryById(@PathVariable ObjectId myId) {
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(myId);
+        if(journalEntry.isPresent()) {
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @DeleteMapping("/{myId}")
+    public ResponseEntity<?> deleteEntryById(@PathVariable ObjectId myId) {/*here we can return any entity*/
+        journalEntryService.deleteById(myId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{myId}")
+    public ResponseEntity<?> updateEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry) {
+        JournalEntry old = journalEntryService.findById(myId).orElse(null);
+        if(old != null) {
+        old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+        old.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent());
+            journalEntryService.saveEntry(newEntry);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+
+}
---
+++ src/main/java/com/Kapoor/journalApp/entity/JournalEntry.java
@@ -0,0 +1,22 @@
+package com.Kapoor.journalApp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "journal_entries")
+@Getter
+@Setter
+//@Data or we can use this instead of these two
+public class JournalEntry {
+
+    @Id //to map as primary key
+    private ObjectId id;
+    private String title;
+    private String content;
+    private LocalDateTime date;
+}
