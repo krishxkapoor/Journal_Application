d3f33ef2d3c208e51536f30d8ff570a13088dd18
krishxkapoor <krishxkapoor@gmail.com>
2025-10-18 13:48:09 +0530

    forth

--- src/main/java/com/Kapoor/journalApp/Config/DbFactory.java
+++ src/main/java/com/Kapoor/journalApp/Config/DbFactory.java
@@ -1,20 +1,15 @@
-package com.Kapoor.journalApp;
+package com.Kapoor.journalApp.Config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-
-public class JournalApplication {
-
-	public static void main(String[] args) {
-
-        SpringApplication.run(JournalApplication.class, args);
-	}
-
+@EnableTransactionManagement
+public class DbFactory {
+    @Bean
+    public PlatformTransactionManager nothing(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+    }
 }
--- src/main/java/com/Kapoor/journalApp/service/JournalEntryService.java
+++ src/main/java/com/Kapoor/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.Kapoor.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,6 +20,7 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String username){
         User user = userService.findByUsername(username);
         journalEntry.setDate(LocalDateTime.now());
